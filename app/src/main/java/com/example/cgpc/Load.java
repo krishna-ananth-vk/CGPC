d5885f7aa505793dbf89cdc1a828ae8a3ff1d799
the-code-builder <krishnaananthvk@gmail.com>
2019-08-17 20:03:11 +0530

    apk build added

--- app/src/main/java/com/example/cgpc/Load.java
+++ app/src/main/java/com/example/cgpc/Load.java
@@ -7,40 +7,33 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class Load extends Activity {
-    private static final int RC_SIGN_IN = 123;
     TextInputEditText email;
     TextInputEditText password;
     TextView login;
-    String e,p;
+    String e, p;
 
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.load);
-
 
 
     }
@@ -52,8 +45,8 @@ public class Load extends Activity {
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user != null){
-            startActivity(new Intent(this,MainActivity.class));
+        if (user != null) {
+            startActivity(new Intent(this, MainActivity.class));
         }
 
 
@@ -66,12 +59,11 @@ public class Load extends Activity {
             public void onClick(View view) {
                 e = email.getText().toString();
                 p = password.getText().toString();
-                if(TextUtils.isEmpty(e) || TextUtils.isEmpty(p)){
+                if (TextUtils.isEmpty(e) || TextUtils.isEmpty(p)) {
                     Toast.makeText(Load.this, "email or password not provided",
                             Toast.LENGTH_LONG).show();
-                }
-                else {
-                    signInUser(e,p);
+                } else {
+                    signInUser(e, p);
                     login.setText("Logging in");
                     login.setEnabled(false);
 
@@ -81,7 +73,8 @@ public class Load extends Activity {
         });
 
     }
-    void signInUser(String e,String p){
+
+    void signInUser(String e, String p) {
         final ProgressDialog progressDialog = new ProgressDialog(Load.this,
                 R.style.AppTheme_Dark_Dialog);
         progressDialog.setIndeterminate(true);
